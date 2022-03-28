997be3813278c76182ecbf0e664f8fde12630d9b
Fernando Cezar Bernardelli <fernando@brainbot.com>
2022-03-28 12:01:28 +0200

    add serverless configs and dependencies

--- app.py
+++ app.py
@@ -2,5 +2,8 @@ from web_app import create_app, socketio
 
 app = create_app()
 
+def run_app(*args):
+    socketio.run(app, host='0.0.0.0', port=8080)
+
 if __name__ == '__main__':
     socketio.run(app)
