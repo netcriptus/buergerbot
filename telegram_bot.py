3cc01f6e7832f02d8aa125a9e1cac1a55a46d745
Fernando Cezar Bernardelli <fernando@brainbot.com>
2022-07-29 13:16:50 +0200

    WIP: implement telegram bot commands

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,16 +1,80 @@
 import os
+from time import sleep
 from dotenv import load_dotenv
-from telegram import Update
-from telegram.ext import CallbackContext, CommandHandler, Updater
+from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
+from telegram.ext import (
+    Application,
+    CommandHandler,
+    ConversationHandler,
+    MessageHandler,
+    filters,
+)
 
 load_dotenv()
-
-def start(update: Update, context: CallbackContext):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
-
 token = os.getenv('TOKEN')
-updater = Updater(token=token, use_context=True)
-dispatcher = updater.dispatcher
-start_handler = CommandHandler('start', start)
-dispatcher.add_handler(start_handler)
-updater.start_polling()
+
+SELECT_SERVICE, ANMELDUNG = range(2)
+AVAILABLE_SERVICES = ["Anmeldung"]
+
+SERVICE_RUNNING = False
+
+async def start(update, _):
+    print(f'At start service. Received {update.message.text}')
+    reply_keyboard = [AVAILABLE_SERVICES]
+    await update.message.reply_text(
+        'Hello, welcome to your Berlin assistant Bot!',
+        reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
+    )
+    print('exiting start')
+    return SELECT_SERVICE
+
+async def anmeldung(update, _):
+    print(f'At anmeldung. Received {update.message.text}')
+    while SERVICE_RUNNING:
+        await update.message.reply_text("Anmeldung erfolgreich!", reply_markup=ReplyKeyboardMarkup([['Stop']]))
+        sleep(10)
+    print('exiting anmeldung')
+    return ConversationHandler.END
+
+async def start_service(update, _):
+    print(f'At start_service. Received {update.message.text}')
+    global SERVICE_RUNNING
+    selected_service = update.message.text
+    if selected_service not in AVAILABLE_SERVICES:
+        reply_keyboard = [AVAILABLE_SERVICES]
+        await update.message.reply_text(
+            "Sorry, I don't know this service. Please select one of the following services:",
+            reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
+        )
+        return SELECT_SERVICE
+    SERVICE_RUNNING = True
+    print('exiting start_service')
+    return ANMELDUNG
+
+async def stop_service(update, _):
+    print(f'At stop_service. Received {update.message.text}')
+    global SERVICE_RUNNING
+    SERVICE_RUNNING = False
+    await update.message.reply_text("Bye!", reply_markup=ReplyKeyboardRemove(),)
+    print('exiting stop_service')
+    return ConversationHandler.END
+
+def main():
+    application = Application.builder().token(token).build()
+
+    conversation_handler = ConversationHandler(
+        entry_points=[CommandHandler("start", start)],
+        states={
+            SELECT_SERVICE: [MessageHandler(filters.Regex('^[' + '|'.join(AVAILABLE_SERVICES) + ']$'), start_service)],
+            ANMELDUNG: [MessageHandler('Anmeldung', anmeldung), CommandHandler("Stop", stop_service)]
+        },
+        fallbacks=[CommandHandler("stop", stop_service)]
+    )
+
+    application.add_handler(conversation_handler)
+    print('Bot is running...')
+    application.run_polling()
+
+
+if __name__ == '__main__':
+    main()
